8a2ddd9b1beb8d0b52e01e692f14cae84dc86b84
MaxCui <cuiye7407@163.com>
2022-08-23 09:14:34 +0800

    comments & numbers & math

---
+++ thehardway/way0822.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+print("hello world")
+print "hello again"
+print "I like trying this."
+print "this is fun"
+print "yay! printing"
+print "i'd much rather you not"
+print "i said do not touch this"
