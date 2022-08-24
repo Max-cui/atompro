1c7cd5822e1861a96ae2f941a5cb1da5a8e0d899
MaxCui <cuiye7407@163.com>
2022-08-24 18:31:19 +0800

    var&printing

---
+++ thehardway/way0824_morevar&printing.py
@@ -0,0 +1,37 @@
+# [value if condition else value for value in variable]
+# {value for value in variable}
+# {key: value for key, value in variable}
+# self.assertIsNotNone(actual, 'message')
+# self.assert_(boolean expression, 'message')
+# self.assertFalse(actual, 'message')
+#
+# class ClassName(object):
+#     """docstring for ."""
+#
+#     def __init__(self, arg):
+#         super(, self).__init__()
+#         self.arg = arg
+# def fname(arg):
+#     pass
+#
+# # DEBUG:
+#
+# def mname(self, arg):
+#     pass
+
+my_name = 'chris.cui'
+my_age = 29
+my_height = 185 # cm
+my_weight = 75 # this is a lie
+my_eyes = 'brown'
+my_teeth = 'white'
+my_hair = 'black'
+
+print "let's talk about %s" % my_name
+print "He is %d cm high" % my_height
+print "He's %d kilo weight" % my_weight
+print "Actually, he is very great"
+print "He got %s hair and %s eyes" % (my_hair, my_eyes)
+print "his teeth are always not very good and %s" % my_teeth
+
+print "If i add %d, %d, %d, and %s" % (my_age, my_height, my_weight, my_eyes)
