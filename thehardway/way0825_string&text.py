69ce5f6f26e9cbef32f62d9fbd15b91eb3d08df3
MaxCui <cuiye7407@163.com>
2022-08-25 13:44:39 +0800

    string&text

---
+++ thehardway/way0825_string&text.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+
+x = "There are %d types of people" % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who knows %s and those who %s" % (binary, do_not)
+
+print x
+print y
+
+print "I said %r." % x
+print "I also said %r." % y
+
+Hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+print joke_evaluation % Hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
